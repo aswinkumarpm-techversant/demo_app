ed5c35feb256adf047773f9d5ab7950b5b044bed
vishnuprakash <vishnu.prakash@techversantinfo.com>
2020-12-28 11:15:57 +0530

    theme editing added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require ("packs/vendor.min")
+require ("packs/app.min")
+require ("packs/jquery-1.10.2")
+require ("packs/jquery.min")
+
+//= require others/get-shit-done.js
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
